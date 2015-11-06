5df5fa92c23435c33ed77ad043eba4a775ed8de6
Jayant Kumar Nayak <jayant.kumar.nayak92@gmail.com>
2015-11-07 01:04:25 +0530

    Jqquery mobile based application

---
+++ js/constant.js
@@ -0,0 +1,35 @@
+﻿
+var constantVal = {
+    currentEmp : " ",
+
+    editEmpItem: "editEmpNameDetails",
+	
+	
+   databaseCreatedStatus : false,
+   circularReferenceUtil : [], 
+   rootEmp : "",
+   flagTreeStatus : false,
+   tblOrgName: "empTable",
+   databaseName:"EmpDatabase",
+   databaseVersion: '1.0',
+   databaseSize: 4*1024*1024
+   
+   
+
+};
+
+
+var message = {
+	validName : "Enter Valid Name",
+	validDeisgnation : "Enter Valid Designation",
+	validManager : "Select Manager",
+	ceoNameMessage: "Enter a valid CEO name",
+	databaseDescription: "Employee DB"
+	
+};
+
+
+
+
+
+
